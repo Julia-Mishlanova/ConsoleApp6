17dbf73b2e3605c33de2448cd9d8bcacf810b8a4
Julia-Mishlanova <mislanovaulia94@gmail.com>
2023-11-13 13:30:22 +0400

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку...");
+            var output = Console.ReadLine();
+
+            Console.WriteLine("Слова, начинающиеся с гласной буквы:");
+            DisplayWordsStartingWithVowel(output);
+
+            Console.ReadKey();
+        }
+
+        public static void DisplayWordsStartingWithVowel(string output)
+        {
+            char[] vowels = { 'a','e','i','o','u' };
+            var words = output.Split(' ');
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                var firstLetter = words[i].FirstOrDefault();
+                var lowerLetter = char.ToLower(firstLetter);
+
+                if (vowels.Contains(lowerLetter))
+                {
+                    Console.WriteLine(words[i]);
+                }
+            }
+        }
+    }
+}
