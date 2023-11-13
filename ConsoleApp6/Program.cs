4f589a0b5608297128364c310702c00216828175
Julia-Mishlanova <mislanovaulia94@gmail.com>
2023-11-13 13:47:46 +0400

    Getting rid of entering empty values

--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ConsoleApp6
 {
     internal class Program
     {
+        static readonly char[] VOWELS = { 'a', 'e', 'i', 'o', 'u' };
         static void Main(string[] args)
         {
             Console.WriteLine("Введите строку...");
+
             var output = Console.ReadLine();
+            while (output == string.Empty) 
+            {
+                Console.WriteLine("Вы ничего не ввели.");
+                output = Console.ReadLine();
+            }
 
             Console.WriteLine("Слова, начинающиеся с гласной буквы:");
             DisplayWordsStartingWithVowel(output);
@@ -18,7 +26,6 @@ namespace ConsoleApp6
 
         public static void DisplayWordsStartingWithVowel(string output)
         {
-            char[] vowels = { 'a','e','i','o','u' };
             var words = output.Split(' ');
 
             for (int i = 0; i < words.Length; i++)
@@ -26,7 +33,7 @@ namespace ConsoleApp6
                 var firstLetter = words[i].FirstOrDefault();
                 var lowerLetter = char.ToLower(firstLetter);
 
-                if (vowels.Contains(lowerLetter))
+                if (VOWELS.Contains(lowerLetter))
                 {
                     Console.WriteLine(words[i]);
                 }
